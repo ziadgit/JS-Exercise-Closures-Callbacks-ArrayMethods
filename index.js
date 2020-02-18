670512e99c2e1a4ad042fd8c86189481152e1a94
Ziad <ziadbc@gmail.com>
2020-02-17 19:31:06 -0800

    pass processsum

--- index.js
+++ index.js
@@ -87,8 +87,8 @@ function processLastItem(stringList, callback) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+function processSum(numberList, callback) {
+  return callback(numberList.reduce((a, b) => a + b, 0))
 }
 
 /**
